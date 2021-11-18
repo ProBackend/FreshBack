adb23a1f7c0677c0703685ae8bb9eb56f278a722
jessra <saravila0762@gmail.com>
2021-11-18 09:02:42 -0430

    Agregada funcionalidad a registro

--- src/controllers/Login.js
+++
@@ -1,6 +0,0 @@
-const cliente = require("../models/cliente")
-const gerente = require("../models/gerente")
-
-module.exports.guardar = async (req, res) => {
-    console.log(req.body)
-}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const Prdt = require("../controllers/Producto")
-const Menu = require("../controllers/menu_dia")
-const Login = require("../controllers/Login")
+const Prdt = require("../controllers/Producto");
+const Menu = require("../controllers/menu_dia");
+const Clientes = require('../models/cliente.js');
 
 /* GET home page. */
-router.get('/Login', function(req, res) {
-    res.render("login")
+router.get('/Login', async (req, res) => {
+  res.render('Login')
 });
-// router.post('/Login', function(req, res) {
-//     console.log('aaa')
-//     console.log(req.body)
-//     res.send('co;oooo')
-// });
+router.get('/Clientes', async (req, res) => {
+    const cliente = await Clientes.find();
+    res.json(cliente);
+});
+router.post('/Login', async (req, res) => {
+  const cliente = new Clientes(req.body);
+  await cliente.save();
+  res.json({
+    status: 'Te has registrado correctamente'
+  })
+});
 
 router.get("/:productos", Prdt.save);
 //Poductos
