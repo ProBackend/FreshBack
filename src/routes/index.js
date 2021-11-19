3213bdc291eb9db5e13410d440f5be99fa5a3f61
jessra <saravila0762@gmail.com>
2021-11-19 14:53:25 -0430

    Traspaso de archivos para evitar error del token y hacer la vista de productos de una vez por odas

--- src/routes/index.js
+++ src/routes/index.js
@@ -43,30 +43,37 @@ router.post('/Login/Iniciar_sesion', Passport.authenticate('local', {
   failureFlash: true
 }));
 
+router.get('/Productos', function(req, res) {
+  res.render('Inventario')
+})
+router.get('/Productos/consulta', async function(req, res) {
+  res.json(await Producto.consultar())
+})
+
 //Poductos
-router.get("/:productos", Prdt.save);
-router.post("/productos", Prdt.guardar);
-router.get("/productos/mostrar", Prdt.mostrar);
-router.get("/productos/editar/:id", Prdt.edit);
-router.post("/productos/editar/:id", Prdt.editar);
-router.get("/productos/delete/:id", Prdt.delete);
+// router.get("/:productos", Prdt.save);
+// router.post("/productos", Prdt.guardar);
+// router.get("/productos/mostrar", Prdt.mostrar);
+// router.get("/productos/editar/:id", Prdt.edit);
+// router.post("/productos/editar/:id", Prdt.editar);
+// router.get("/productos/delete/:id", Prdt.delete);
 
 //Menu del día
-router.post("/menu", Menu.guardar);
-router.get("/menu/mostrar", Menu.mostrar);
-router.get("/menu/editar/:id", Menu.edit);
-router.post("/menu/editarm/:id", Menu.editar);
-router.get("/menu/delete/:id", Menu.delete);
+// router.post("/menu", Menu.guardar);
+// router.get("/menu/mostrar", Menu.mostrar);
+// router.get("/menu/editar/:id", Menu.edit);
+// router.post("/menu/editarm/:id", Menu.editar);
+// router.get("/menu/delete/:id", Menu.delete);
 
 //Producto del día
-router.post("/productodia", Pr_dia.guardar);
-router.get("/productodia/mostrar", Pr_dia.mostrar);
-router.get("/productodia/editar/:id", Pr_dia.edit);
-router.post("/productodia/editar/:id", Pr_dia.editar);
-router.get("/productodia/delete/:id", Pr_dia.delete);
+// router.post("/productodia", Pr_dia.guardar);
+// router.get("/productodia/mostrar", Pr_dia.mostrar);
+// router.get("/productodia/editar/:id", Pr_dia.edit);
+// router.post("/productodia/editar/:id", Pr_dia.editar);
+// router.get("/productodia/delete/:id", Pr_dia.delete);
 
-router.get('/AcercadeNosotros', function(req, res) {
-  res.render('AcercadeNosotros')
-})
+// router.get('/AcercadeNosotros', function(req, res) {
+//   res.render('AcercadeNosotros')
+// })
 
 module.exports = router;
