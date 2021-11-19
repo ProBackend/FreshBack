6c295d3b1ae725fe18f090609ac3b61d6020f6f2
jessra <saravila0762@gmail.com>
2021-11-19 14:56:27 -0430

    Traspaso de archivos para evitar error del token y hacer la vista de productos de una vez por odas

--- src/controllers/Producto.js
+++ src/controllers/Producto.js
@@ -1,79 +1,101 @@
 const Producto = require("../models/productos");
-
-module.exports.save = (req, res) => {
-  const url = req.params.productos
-  if (url == "productodia") {
-    res.render("productodia", { url })
-  } else {
-    res.render("Productos", { url })
+class pro{
+  constructor(req) {}
+  async consultar() {
+    const consulta = await Producto.find()
+    return consulta
+  }
+  // pruebassa(token) {
+  //   if(token){
+  //     token = false
+  //   } else{
+  //     token = true;
+  //   }
+  //   console.log('asdasdasAAA')
+  //   return token
+  // }
+  async guardar(req, res) {
+    const pro = new Producto(re.body);
+    await pro.save();
+    const mensaje = 'Te has registrado correctamente'
+    return mensaje
   }
 }
 
-module.exports.guardar = async(req, res) => {
-  const { nombre, ingredientes, precio } = req.body;
+// module.exports.save = (req, res) => {
+//   const url = req.params.productos
+//   if (url == "productodia") {
+//     res.render("productodia", { url })
+//   } else {
+//     res.render("Productos", { url })
+//   }
+// }
 
-  if (!nombre || !ingredientes || !precio || !req.file) {
-    return res.send("Ingrese la información correctamente")
-  }
+// module.exports.guardar = async(req, res) => {
+//   const { nombre, ingredientes, precio } = req.body;
 
-  const producto = new Producto({
-    nombre: nombre,
-    ingredientes: ingredientes,
-    precio: precio,
-    filename: req.file.filename,
-    path: "/uploads/" + req.file.filename,
-    orinalname: req.file.originalname,
-    mimetype: req.file.mimetype,
-    size: req.file.size
-  })
+//   if (!nombre || !ingredientes || !precio || !req.file) {
+//     return res.send("Ingrese la información correctamente")
+//   }
 
-  await producto.save()
-  return res.send("Guardado con exito")
-};
+//   const producto = new Producto({
+//     nombre: nombre,
+//     ingredientes: ingredientes,
+//     precio: precio,
+//     filename: req.file.filename,
+//     path: "/uploads/" + req.file.filename,
+//     orinalname: req.file.originalname,
+//     mimetype: req.file.mimetype,
+//     size: req.file.size
+//   })
 
-module.exports.mostrar = async(req, res) => {
-  const p = "productos"
-  const mostrart = await Producto.find();
-  res.render("pmostrar", { mostrart, p });
-}
+//   await producto.save()
+//   return res.send("Guardado con exito")
+// };
 
-module.exports.edit = async(req, res) => {
-  const p = "productos"
-  const id = req.params.id
-  const mostrar_id = await Producto.findById(id)
-  if (id == null) {
-    res.send("No existe ese código");
-    res.end();
-  } else {
-    res.render("peditar", { mostrar_id, p })
-  }
-}
+// module.exports.mostrar = async(req, res) => {
+//   const p = "productos"
+//   const mostrart = await Producto.find();
+//   res.render("pmostrar", { mostrart, p });
+// }
 
-module.exports.editar = async(req, res) => {
-  const valor = req.params.id;
-  if (req.file) {
-    await Producto.updateOne({ id: valor }, {
-      nombre: req.body.nombre,
-      ingredientes: req.body.ingredientes,
-      precio: req.body.precio,
-      filename: req.file.filename,
-      path: "/uploads/" + req.file.filename,
-      orinalname: req.file.originalname,
-      mimetype: req.file.mimetype,
-      size: req.file.size
-    })
-  } else {
-    await Producto.updateOne({ id: valor }, {
-      nombre: req.body.nombre,
-      ingredientes: req.body.ingredientes,
-      precio: req.body.precio
-    })
-  }
-  return res.send("Se ha editado correctamente");
-}
+// module.exports.edit = async(req, res) => {
+//   const p = "productos"
+//   const id = req.params.id
+//   const mostrar_id = await Producto.findById(id)
+//   if (id == null) {
+//     res.send("No existe ese código");
+//     res.end();
+//   } else {
+//     res.render("peditar", { mostrar_id, p })
+//   }
+// }
 
-module.exports.delete = async(req, res) => {
-  const codigon = req.params.id;
-  await Producto.deleteOne({ id: codigon });
-  return res.send("Se ha eliminado");
-}
+// module.exports.editar = async(req, res) => {
+//   const valor = req.params.id;
+//   if (req.file) {
+//     await Producto.updateOne({ id: valor }, {
+//       nombre: req.body.nombre,
+//       ingredientes: req.body.ingredientes,
+//       precio: req.body.precio,
+//       filename: req.file.filename,
+//       path: "/uploads/" + req.file.filename,
+//       orinalname: req.file.originalname,
+//       mimetype: req.file.mimetype,
+//       size: req.file.size
+//     })
+//   } else {
+//     await Producto.updateOne({ id: valor }, {
+//       nombre: req.body.nombre,
+//       ingredientes: req.body.ingredientes,
+//       precio: req.body.precio
+//     })
+//   }
+//   return res.send("Se ha editado correctamente");
+// }
+
+// module.exports.delete = async(req, res) => {
+//   const codigon = req.params.id;
+//   await Producto.deleteOne({ id: codigon });
+//   return res.send("Se ha eliminado");
+// }
