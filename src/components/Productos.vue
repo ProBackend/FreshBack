344a11a598bdae586e1bf865fe30c207083f235d
jessra <saravila0762@gmail.com>
2021-11-20 13:20:37 -0430

    Arreglo de cards

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -4,30 +4,25 @@
       <div class="dropdown">
         <button class="dropbtn">Agregar</button>
         <div class="dropdown-content">
-          <a href="#">Productos Regulares</a>
+          <a :href="modalPro">Productos Regulares</a>
           <a href="#">Productos del día</a>
           <a href="#">Menúa del día</a>
         </div>
       </div>
-      <div class="card" v-for="p in productos" :key="p.nombre">
-        <div class="card-columns">
-          {{p}}
-            <div class="card animated fadeInDown">
-              <img :src="p.path" class="card-img-top" :alt="p.filename">
-              <div class="card-body">
-                <h5 class="card-title">
-                  {{p.nombre}}
-                </h5>
-                <p class="card-text">
-                  {{p.ingredientes}}
-                </p>
-                <p class="card-text">
-                  {{p.precio}}
-                </p>
-                <a href="/<%=p %>/delete/<%= mostrart.id%>" class="btn btn-danger btn-block">Delete</a>
-                <a href="/<%=p %>/editar/<%= mostrart.id%>" class="btn btn-primary">Editar</a>
-              </div>
-            </div>
+      <div class="card" style="width: 20rem" v-for="p in productos" :key="p.nombre">
+        <img :src="p.path" class="card-img-top" :alt="p.filename">
+        <div class="card-body text-center">
+          <h5 class="card-tittle">
+            {{p.nombre}}
+          </h5>
+        </div>
+        <ul class="list-group list-group-flush text-center">
+          <li class="list-group-item">{{p.ingredientes}}</li>
+          <li class="list-group-item">{{p.precio}}</li>
+        </ul>
+        <div class="card-body">
+          <a href="/<%=p %>/delete/<%= mostrart.id%>" class="btn btn-danger btn-block">Delete</a>
+          <a href="/<%=p %>/editar/<%= mostrart.id%>" class="btn btn-primary">Editar</a>
         </div>
       </div>
     </div>
